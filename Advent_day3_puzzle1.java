3833b0031b84fcbd0d39440df931aa42852889a4
harrison.knight <harrison.g.r.knight@gmail.com>
2025-09-02 15:19:59 -0500

    started day3

--- Advent_day3_puzzle1.java
+++ Advent_day3_puzzle1.java
@@ -1,5 +1,44 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Advent_day3_puzzle1 {
     public static void main(String[] args) {
-        
+        List<String> numbers = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader("day3.txt"))) {
+            String line;
+            StringBuilder contentBuilder = new StringBuilder();
+
+            while ((line = reader.readLine()) != null) {
+                contentBuilder.append(line);
+            }
+            String fileContent = contentBuilder.toString();
+            searchInput(fileContent, numbers);
+    } catch (IOException e) {
+        e.printStackTrace();
     }
+
+    }
+
+    static void searchInput(String line, List<String> numbers) {
+        String phrase = line;
+        Pattern pattern = Pattern.compile("mul\\(\\d+\\,\\d+\\)");
+        Matcher matcher = pattern.matcher(phrase);
+
+    
+
+        while (matcher.find()){
+            System.out.println(matcher.group());
+            String cleanedUp = matcher.group().replaceAll("mul()[^0-9]", "").replaceAll("\\)", "");
+            numbers.add(cleanedUp);
+            System.out.println(numbers);
+        }
+    }
+
+
 }
