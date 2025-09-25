adc4e2a9013ed4a59823d70bd1a089cbf261fe9b
harrison.knight <harrison.g.r.knight@gmail.com>
2025-09-25 14:53:57 -0500

    day 5 progress

--- adventofcode/Advent_day5_puzzle1.java
+++ adventofcode/Advent_day5_puzzle1.java
@@ -3,27 +3,45 @@ package adventofcode;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class Advent_day5_puzzle1 {
 
     public static void main(String[] args) {
-        HashMap<Integer, Integer> rules = new HashMap<>();
+        HashMap<Integer, ArrayList<Integer>> rules = new HashMap<>();
+        List<List<Integer>> pages = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\harrison.knight\\VS Code Projects\\resources\\day5example.txt"))) {
             String line;
+            // Pattern.pattern = Pattern.compile("\\d{1,2}\\|\\d{1,2}");
+            // Matcher matcher = pattern.matcher(line);
+
             while ((line = reader.readLine()) != null) {
+                
                 String[] ruleArray = line.split("\\|");
                 int[] ruleIntArray = new int[ruleArray.length];
-                for (int i = 0; i < ruleArray.length; i++) {
-                    ruleIntArray[i] = Integer.parseInt(ruleArray[i]);
-                    if rules.containsKey(ruleIntArray[0])
-                    rules.put(ruleIntArray[0], ruleIntArray[1]);
-                }
-                
-                rules.put(ruleIntArray[0], ruleIntArray[1]);
-            }
+                    ruleIntArray[0] = Integer.parseInt(ruleArray[0]);
+                    ruleIntArray[1] = Integer.parseInt(ruleArray[1]);
+                    if (rules.containsKey(ruleIntArray[0])) {
+                        rules.get(ruleIntArray[0]).add(ruleIntArray[1]);
+                    }else {
+                        ArrayList<Integer> ruleValues = new ArrayList<>();
+                        ruleValues.add(ruleIntArray[1]);
+                        rules.put(ruleIntArray[0], ruleValues);
+                    if (line.trim().isEmpty()) {
+                        continue;
+
+                        }
+                    
+                    }System.out.println(rules);
+            } 
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    static void readTheRest(String line) {
+        
     }
 }
