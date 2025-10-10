f94f698f3c6ea0730b252bc2b83046c129afe239
harrison.knight <harrison.g.r.knight@gmail.com>
2025-10-10 11:05:28 -0500

    day 6 pt 1 done

--- adventofcode/Advent_day6_puzzle1.java
+++ adventofcode/Advent_day6_puzzle1.java
@@ -10,7 +10,7 @@ public class Advent_day6_puzzle1 {
 
     public static void main(String[] args) {
         List<List<Character>> grid = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\harrison.knight\\VS Code Projects\\resources\\day6example.txt"))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\harrison.knight\\VS Code Projects\\resources\\day6.txt"))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 List<Character> row = new ArrayList<>();
@@ -47,29 +47,35 @@ public class Advent_day6_puzzle1 {
                 }
             }
         }
-        System.out.println(grid);
+        // System.out.println(grid);
     }
 
     static void turn(List<List<Character>> grid, int[][] directionsMap, int currentCol, int currentRow, char replacement, char obstacle, int direction, int uniquePath) {
         direction = 0; //reset back to heading up/north
+        loop:
         for (int i = currentCol; i < grid.size(); currentCol++) {
             for (int j = currentRow; currentRow < grid.get(currentCol).size(); currentRow++) {
-                while (currentRow < grid.get(currentCol).size() && currentCol < grid.size()) {
+                grid.get(currentCol).set(currentRow, replacement); //replace current position with 'X'
+                while (currentRow < grid.get(currentCol).size() || currentCol < grid.size()) {
                     try {
                         if (grid.get(currentCol + directionsMap[direction][1]).get(currentRow + directionsMap[direction][0]) != obstacle) { //if obstacle is not found in next step
-                            grid.get(currentCol).set(currentRow, replacement); //replace current position with 'X'
+                            grid.get(currentCol+ directionsMap[direction][1]).set(currentRow + directionsMap[direction][0], replacement); //replace current position with 'X'
                             currentRow = currentRow + directionsMap[direction][0];
                             currentCol = currentCol + directionsMap[direction][1];
+                            // printGrid(grid);
                         } else { //if obstacle is found
                             if (direction == 3) { //if currently heading left/west
                                 direction = 0; //reset back to heading up/north
+                                // printGrid(grid);
                             } else {
                                 direction++;
+                                // printGrid(grid);
                             }
 
                         }
                     } catch (IndexOutOfBoundsException e) {
                         countUniquePaths(grid);
+                        break loop;
                     }
                 }
             }
